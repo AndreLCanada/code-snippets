b741684479bd92ecf33e3ec428d294590b20e423
Andre Lacke <andrelacketech@gmail.com>
2022-10-26 12:40:29 -0700

    Fix: Head placement

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,9 @@
 import React, { useState } from 'react'
 import '../styles/globals.css'
 import Layout from '../components/Layout'
-import { usePostHog } from 'next-use-posthog'
+
 function MyApp({ Component, pageProps }) {
-  usePostHog(process.env.NEXT_PUBLIC_PH_KEY, { 
-    api_host: 'https://app.posthog.com',
-  })
+  
   return (
   <Layout>
     <Component {...pageProps} />
