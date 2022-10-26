9057da16af6197dea50f9a9bfb23ea4f1933e0c7
Andre Lacke <andrelacketech@gmail.com>
2022-10-26 12:15:23 -0700

    Fix: ud

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
 import '../styles/globals.css'
 import Layout from '../components/Layout'
-
+import { usePostHog } from 'next-use-posthog'
 function MyApp({ Component, pageProps }) {
- 
+  usePostHog(process.env.NEXT_PUBLIC_PH_KEY, { api_host: 'https://app.posthog.com', loaded: (posthog) => {
+    if (process.env.NODE_ENV === 'development') posthog.opt_out_capturing()
+  }, })
   return (
   <Layout>
     <Component {...pageProps} />
